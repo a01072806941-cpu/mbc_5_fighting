5a030a4a769d87dc501c12988cacf2c7b4f93b0f
a01072806941-cpu <a01072806941@gmail.com>
2025-10-22 12:00:01 +0900

    스크립트 수정hello world

---
+++ Assets/Script_test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello");
+        Debug.Log("world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
