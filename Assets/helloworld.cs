09c1582ab89d09c162b10a27b29c4c2666e5770f
a01072806941-cpu <a01072806941@gmail.com>
2025-10-22 11:46:14 +0900

    Update helloworld.cs

--- Assets/helloworld.cs
+++ Assets/helloworld.cs
@@ -7,7 +7,7 @@ public class helloworld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Çï·Î¿ì¿ùµå");
     }
 
     // Update is called once per frame
