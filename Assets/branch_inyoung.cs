936358da3fcd8043e8a629b7049691c3b787adca
a01072806941-cpu <a01072806941@gmail.com>
2025-10-22 14:59:08 +0900

    Update branch_inyoung.cs

--- Assets/branch_inyoung.cs
+++ Assets/branch_inyoung.cs
@@ -7,7 +7,7 @@ public class branch_inyoung : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("branch_001");
     }
 
     // Update is called once per frame
